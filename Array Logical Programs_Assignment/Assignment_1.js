c4f8b99da4852c619de5c064c4938ea40cec2e07
04schaudhari <mandesayali04@gmail.com>
2024-09-02 22:41:54 +0530

    Array Logical Program
    Array Logical Program 1 and 2

---
+++ Array Logical Programs_Assignment/Assignment_1.js
@@ -0,0 +1,16 @@
+// 1. Javascript program to print the number of an element present in array.
+let arr = [1,2,3,4,5,6];
+
+function printNumberOfElements(arr){
+
+
+   console.log(`Given Array is:[${arr}]`);
+   
+    console.log(`Number of elements in array is: ${arr.length}`);
+    
+
+}
+
+
+printNumberOfElements(arr);
+
